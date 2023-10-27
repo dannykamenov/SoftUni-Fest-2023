806cee91b56c3d8261e98533a1b285863eeef14d
Daniel Kamenov <sorostmcoc@gmail.com>
2023-10-27 14:32:16 +0300

    lots of updates

--- FE/payment-solutions/src/app/auth/register/register.component.ts
+++ FE/payment-solutions/src/app/auth/register/register.component.ts
@@ -1,21 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { matchPasswordGroup } from 'src/app/shared/validators/match-password-validator';
 import { Auth, createUserWithEmailAndPassword } from '@angular/fire/auth';
 import { AuthService } from 'src/app/shared/services/auth.service';
 import { Router } from '@angular/router';
 import { getAuth, updateProfile } from 'firebase/auth';
+import { ToggleService } from 'src/app/shared/services/toggle.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
 
   error: string = '';
+  isToggled: boolean;
 
   form = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(4)]],
     email: [
       '',
       [Validators.required, Validators.email, Validators.minLength(6)],
@@ -29,10 +32,10 @@ export class RegisterComponent {
     ),
   });
 
-  constructor(public authService: AuthService, private fb: FormBuilder,  private router: Router) {}
+  constructor(public authService: AuthService, private fb: FormBuilder,  private router: Router, private toggleService: ToggleService) {}
 
   registerMe() {
-    const { email, pass: {password} = {} } = this.form.value;
+    const { name, email, pass: {password} = {} } = this.form.value;
     this.authService.SignUp(email!, password!).then((result) => {
       if (result) {
         let code = result.code;
@@ -58,6 +61,12 @@ export class RegisterComponent {
         displayName: email?.split('@')[0],
         photoURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Unknown_person.jpg/434px-Unknown_person.jpg'
       });
+    });
+  }
+
+  ngOnInit() {
+    this.toggleService.toggle$.subscribe(value => {
+      this.isToggled = value;
     });
   }
 }
