f89e0972ece0853b02d518062f7c8a159d993b32
Daniel Kamenov <sorostmcoc@gmail.com>
2023-10-28 10:39:14 +0300

    add stripe payment

--- FE/payment-solutions/src/app/elteck/product-page/product-page.component.ts
+++ FE/payment-solutions/src/app/elteck/product-page/product-page.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiService } from 'src/app/shared/services/api.service';
+import { StripeCardNumberComponent } from 'ngx-stripe';
+import {StripeCardElementOptions, StripeElementsOptions} from '@stripe/stripe-js'
+import { NgxStripeModule } from 'ngx-stripe';
 
 @Component({
   selector: 'app-product-page',
@@ -12,6 +15,28 @@ export class ProductPageComponent {
   isLoading = true;
   isNotLoading = false;
   productInfo: any;
+
+@ViewChild(StripeCardNumberComponent) card: StripeCardNumberComponent;
+
+public cardOptions: StripeCardElementOptions = {
+    style: {
+      base: {
+        fontWeight: 400,
+        fontFamily: 'Circular',
+        fontSize: '14px',
+        iconColor: '#666EE8',
+        color: '#002333',
+        '::placeholder': {
+          color: '#919191',
+        },
+      },
+    },
+  };
+
+public elementsOptions: StripeElementsOptions = {
+    locale: 'en',
+  };
+
 
   constructor(private api: ApiService, private router: Router) {
     this.api.getProduct(this.router.url.split('/')[2]).subscribe((res) => {
