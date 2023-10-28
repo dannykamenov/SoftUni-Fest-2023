e3af86e0984536e8e0694f2b867490bfc1b491ab
Daniel Kamenov <sorostmcoc@gmail.com>
2023-10-28 10:12:25 +0300

    product page update

--- FE/payment-solutions/src/app/elteck/product-page/product-page.component.ts
+++ FE/payment-solutions/src/app/elteck/product-page/product-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from 'src/app/shared/services/api.service';
 
 @Component({
   selector: 'app-product-page',
@@ -6,5 +8,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-page.component.scss']
 })
 export class ProductPageComponent {
+
+  isLoading = true;
+  isNotLoading = false;
+  productInfo: any;
+
+  constructor(private api: ApiService, private router: Router) {
+    this.api.getProduct(this.router.url.split('/')[2]).subscribe((res) => {
+      this.productInfo = res;
+      this.isLoading = false;
+      this.isNotLoading = true;
+    });
+   }
 
 }
